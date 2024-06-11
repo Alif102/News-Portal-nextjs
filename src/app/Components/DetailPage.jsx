a03dd12f4dbfa14e409fb0cf468d289c0cfe384c
jimkhan <jim.khan880@gmail.com>
2024-06-11 15:02:33 +0600

    fix(dynamicUrl): added encoded dynamic urls

--- src/app/Components/DetailPage.jsx
+++ src/app/Components/DetailPage.jsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 import React, { useEffect } from 'react';
 import PostBody from './Shared/Postbody';
 import Image from 'next/image';
@@ -8,83 +8,79 @@ import Head from 'next/head';
 export const metadata = {
   title: 'detailllll',
   description: 'descriiiiptionnn',
-  
 };
 
-
 const DetailPage = ({ post }) => {
-  const imageUrl = `https://admin.desh365.top/public/storage/post-image/${post?.image}`;
-  const currentUrl = 'https://newsportalnextjs.vercel.app'
+  const router = useRouter();
+  const currentUrl = `https://newsportalnextjs.vercel.app${router.asPath}`;
+  const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+    currentUrl,
+  )}`;
   useEffect(() => {
-
     document.title = post?.title || 'news detail';
   }, [post]);
 
   if (!post) {
     return <div>Loading...</div>;
   }
-  console.log(post)
-
+  console.log(post);
 
   return (
     <div>
-     
       <Head>
         <title>titleeeeee</title>
-        <meta property="og:title" content='page details'/>
-        <meta property="og:image" content='https://admin.desh365.top/public/storage/post-image/4598_1716725277.webp' />
-        <meta property="og:url" content='https://newsportalnextjs.vercel.app/Pages/post/16' />
-        <meta property="og:type" content="article" />
-   
+        <meta property='og:title' content='page details' />
+        <meta
+          property='og:image'
+          content='https://admin.desh365.top/public/storage/post-image/4598_1716725277.webp'
+        />
+        <meta
+          property='og:url'
+          content='https://newsportalnextjs.vercel.app/Pages/post/16'
+        />
+        <meta property='og:type' content='article' />
       </Head>
 
-      <div className='p-2 space-y-5' >
-        <a
-          href={`https://www.facebook.com/sharer/sharer.php?u=${(currentUrl)}`}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
+      <div className='p-2 space-y-5'>
+        <a href={shareUrl} target='_blank' rel='noopener noreferrer'>
           <button className='b bg-blue-600 p-2 text-white rounded-lg'>
             Share on Facebook
           </button>
         </a>
 
-
         <h1 className='f text-[22px]  font-bold'> {post?.title} </h1>
 
-
         {/* <img className='h-[400px] w-full rounded-md' src={imageUrl} alt={post?.title} /> */}
-        <div className='rounded-md overflow-hidden relative' style={{ height: '360px', width: '100%' }}>
+        <div
+          className='rounded-md overflow-hidden relative'
+          style={{ height: '360px', width: '100%' }}>
           <Image
             src={imageUrl}
             alt={post?.title || 'Default Alt Text'}
-            layout="fill"
-            objectFit="cover"
+            layout='fill'
+            objectFit='cover'
           />
         </div>
 
         <PostBody postBody={post?.post_body} />
-
       </div>
-
-
-
-
     </div>
-  )
-}
+  );
+};
 
 export default DetailPage;
 
-
-{/* <Head>
+{
+  /* <Head>
         <title>{post?.title}</title>
         <meta property="og:title" content={post?.title} />
         <meta property="og:description" content={post?.Category_name} />
         <meta property="og:image" content={imageUrl} />
         <meta property="og:url" content={window.location.href} />
-      </Head> */}
-{/* 
+      </Head> */
+}
+{
+  /* 
       <div className='flex flex-row gap-5 justify-center mt-7'>
         <a
           href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`}
@@ -98,5 +94,5 @@ export default DetailPage;
         <Link href='/'>
           <button className='b bg-purple-300 p-2 rounded-lg'>Go Home</button>
         </Link>
-      </div> */}
-
+      </div> */
+}
